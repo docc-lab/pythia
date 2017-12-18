e1bfdae2e0bd993162ea22b968918e0d648ca841
Sebastian Wicki <swicki@inf.ethz.ch>
2017-12-18 15:08:57 +0100

    Clean up and document stats module
    Signed-off-by: Sebastian Wicki <swicki@inf.ethz.ch>

--- src/operators/stats.rs
+++ src/operators/stats.rs
@@ -1,222 +1,199 @@
+//! A collection of various basic statistical Timely operators.
+//!
+//! These operators are intended as building blocks for custom operators running
+//! on top the stream of reconstructed user-sessions.
+
 use std::collections::HashMap;
 use std::hash::Hash;
 use std::marker::Sync;
-use std::fmt;
 
-use timely::ExchangeData;
+use timely::{Data, ExchangeData};
 use timely::dataflow::{Stream, Scope};
 use timely::dataflow::channels::pact::{Exchange, Pipeline};
 use timely::dataflow::operators::{Map, Unary};
 use timely::dataflow::operators::aggregation::Aggregate;
 use timely::progress::nested::product::Product;
-use timely::progress::timestamp::RootTimestamp;
+use timely::progress::timestamp::{Timestamp, RootTimestamp};
 
 use SessionizableMessage;
 
 use super::hash_code;
 use super::MessagesForSession;
 
-pub trait CountNumFragmentsPerSessionPerEpoch<S: Scope> {
-    fn count_num_fragments_per_session(&self) -> Stream<S, Vec<(u64, u64)>>;
+/// Computes a histogram of the values observed per epoch.
+///
+/// This operator takes a custom `discretizer` function, i.e. a function which extracts
+/// a nominal variable (bin) `V` from a given input `K`. The frequency within each bin is
+/// reported per epoch.
+pub trait Histogram<S: Scope, K, V> {
+    type Epoch: Timestamp;
+
+    /// Returns a stream of `(timestamp, (bin, frequency))`
+    fn histogram<F>(&self, discretizer: F) -> Stream<S, (Self::Epoch, Vec<(V, u64)>)>
+        where F: Fn(&K) -> V + 'static;
 }
 
-pub trait Histogram<S: Scope, K: ::timely::Data, V: ExchangeData + Hash + Eq> {
-    // Returns a stream of (timestamp, value-counts)
-    fn histogram<F: Fn(&K) -> V + 'static>(
-        &self,
-        discretizer: F,
-    ) -> Stream<S, (u64, Vec<(V, u64)>)>;
-}
+impl<S, K, V, T> Histogram<S, K, V> for Stream<S, K>
+    where S: Scope<Timestamp = Product<RootTimestamp, T>>,
+          K: Data,
+          V: ExchangeData + Hash + Eq + Sync,
+          T: Timestamp,
+{
+    type Epoch = T;
 
-pub trait TopK<S: Scope, K: ::timely::Data, V: ExchangeData + Hash + Eq> {
-    // Returns a stream of (timestamp, value-counts)
-    fn topk<F: Fn(&K) -> V + 'static>(
-        &self,
-        discretizer: F,
-        topk: u64,
-    ) -> Stream<S, (u64, Vec<(V, u64)>)>;
-}
+    fn histogram<F>(&self, discretizer: F) -> Stream<S, (Self::Epoch, Vec<(V, u64)>)>
+        where F: Fn(&K) -> V + 'static
+    {
+        let exchange = Exchange::new(move |_| 0);
+        let mut value_counter: HashMap<T, HashMap<V, u64>> = HashMap::new();
 
-pub trait FinalHistogram<S: Scope, V: ExchangeData + Hash + Eq> {
-    fn final_histogram(&self) -> Stream<S, (u64, Vec<(V, u64)>)>;
-}
-
-// Assume everything is colocated here
-impl<
-    S: Scope<Timestamp = Product<RootTimestamp, u64>>,
-    V: ExchangeData + Hash + Eq,
-> FinalHistogram<S, V> for Stream<S, Vec<(V, u64)>> {
-    fn final_histogram(&self) -> Stream<S, (u64, Vec<(V, u64)>)> {
-        let exchange = Exchange::new(|_| 0);
-        let mut value_counter: HashMap<S::Timestamp, HashMap<V, u64>> = HashMap::new();
-        self.unary_notify(exchange, "histogram", vec![], move |input,
-              output,
-              notificator| {
-            input.for_each(|epoch, values| {
-                let histogram_for_epoch = value_counter
-                    .entry(epoch.time().clone())
-                    .or_insert_with(|| {
-                        notificator.notify_at(epoch);
-                        HashMap::new()
-                    });
-                for value_counts in values.drain(..) {
-                    for value in value_counts {
-                        let value_count = histogram_for_epoch.entry(value.0).or_insert(0);
-                        *value_count += value.1;
-                    }
-                }
-            });
-
-            notificator.for_each(|ts, _count, _notify| {
-                let values: Vec<(V, u64)> =
-                    value_counter.remove(&ts).unwrap().into_iter().collect();
-                output.session(&ts).give((ts.inner, values));
-            });
-        })
-    }
-}
-
-// Workers compute partial counts per epoch (stage1), which are then aggregated
-// by worker 0 to produce the global counts (stage2) for each epoch
-impl<
-    S: Scope<Timestamp = Product<RootTimestamp, u64>>,
-    K: ::timely::Data + fmt::Debug,
-    V: ExchangeData + Hash + Eq + Sync + fmt::Debug,
-> TopK<S, K, V> for Stream<S, K> {
-    fn topk<F: Fn(&K) -> V + 'static>(
-        &self,
-        discretizer: F,
-        topk: u64,
-    ) -> Stream<S, (u64, Vec<(V, u64)>)> {
-
-        // suffle entries
-        let exchange = Exchange::new(move |x| hash_code(x));
-
-        let large_timestamp = RootTimestamp::new(u64::max_value() / 2);
-
-        //self.inspect(|x| println!("TopK input: {:?}",x));
-        let mut value_counter: HashMap<u64, HashMap<V, u64>> = HashMap::new();
-        // count number of occurences for each entry per epoch
-        let stage1 = self.map(move |k: K| discretizer(&k)).unary_notify(
-            exchange,
-            "histogram1",
-            vec![large_timestamp],
+        self.map(move |k: K| discretizer(&k)).unary_notify(
+            exchange, "histogram", vec![],
             move |input, output, notificator| {
-                // map: epoch -> tree signature -> number of occurences in epoch
+                // map: epoch -> category -> number of occurrences in epoch
                 input.for_each(|epoch, values| {
                     notificator.notify_at(epoch.clone());
-                    let epoch_hashmap = value_counter.entry(epoch.time().inner).or_insert_with(
-                        HashMap::new,
-                    );
+                    let epoch_hashmap = value_counter
+                        .entry(epoch.time().inner.clone())
+                        .or_insert_with(HashMap::new);
                     for value in values.drain(..) {
                         let value_count = epoch_hashmap.entry(value).or_insert(0);
                         *value_count += 1;
                     }
                 });
-                //if value_counter.len() > 0 {
-                //    println!("Partial HashMap size: {}",value_counter.len());
-                //}
+
                 // spit counts for each epoch upon notification
                 notificator.for_each(|ts, _count, _notify| {
-                    let ov = value_counter.remove(&ts.time().inner); //.expect("Hist1 Notify");
-                    //println!("Epoch: {}, Values: {}",ts.time().inner,ov.is_some());
-                    if ov.is_some() {
-                        let v = ov.unwrap();
-                        let values: Vec<(V, u64)> =
-                            v.iter().map(|(k, v)| (k.clone(), *v)).collect();
-                        //if value_counter.len() > 0 {
-                        //  println!("Partial HashMap size: {}",value_counter.len());
-                        //}
-                        output.session(&ts).give(values);
+                    let ov = value_counter.remove(&ts.time().inner);
+                    if let Some(v) = ov {
+                        let values: Vec<(V, u64)> = v.into_iter().collect();
+                        output.session(&ts).give((ts.inner.clone(), values));
                     }
-                });
-            },
-        );
-
-        let mut aggregated_per_epoch = HashMap::new();
-        // send partial counts to worker 0 and aggregate them per epoch
-        let stage2 = stage1.unary_notify(Exchange::new(move |_| 0), "histogram2", vec![large_timestamp], move |input, output, notificator| {
-            input.for_each(|epoch, values| {
-                notificator.notify_at(epoch.clone());
-                let epoch_data = aggregated_per_epoch.entry(epoch.time().inner).or_insert_with(HashMap::new);
-                for value in values.drain(..) {
-                    for &(ref v, count) in &value {
-                        let x = epoch_data.entry(v.clone()).or_insert(0);
-                        *x += count;
-                    }
-                }
-            });
-            //println!("HashMap size: {}",aggregated_per_epoch.len());
-            // spit top-k results for each epoch upon notification
-            notificator.for_each(|ts, _count, _notify| {
-                let oepoch_data = aggregated_per_epoch.remove(&ts.time().inner);//.expect("Hist2 Notify");
-                if oepoch_data.is_some() {
-                    let  epoch_data = oepoch_data.unwrap();
-                    let mut values : Vec<(V, u64)> = epoch_data.iter().map(|(k, v)| (k.clone(), *v)).collect();
-                    values.sort_by(|a, b| b.1.cmp(&a.1));
-                    values.truncate(topk as usize);
-                    //println!("Values len: {}",values.len());
-                    output.session(&ts).give((ts.inner, values));
-                }
-            });
-        });
-        //stage2.inspect(|x| println!("TopK output: {:?}",x));
-        stage2
-    }
-}
-
-impl<
-    S: Scope<Timestamp = Product<RootTimestamp, u64>>,
-    K: ::timely::Data,
-    V: ExchangeData + Hash + Eq + Sync,
-> Histogram<S, K, V> for Stream<S, K> {
-    fn histogram<F: Fn(&K) -> V + 'static>(
-        &self,
-        discretizer: F,
-    ) -> Stream<S, (u64, Vec<(V, u64)>)> {
-        let exchange = Exchange::new(move |_| 0);
-        let mut value_counter: HashMap<V, u64> = HashMap::new();
-        let mut num_values_seen: u64 = 0;
-        let large_timestamp = RootTimestamp::new(u64::max_value() / 2);
-        self.map(move |k: K| discretizer(&k)).unary_notify(
-            exchange,
-            "histogram",
-            vec![large_timestamp],
-            move |input, output, notificator| {
-                input.for_each(|epoch, values| {
-                    notificator.notify_at(epoch);
-                    num_values_seen += 1;
-                    for value in values.drain(..) {
-                        let value_count = value_counter.entry(value).or_insert(0);
-                        *value_count += 1;
-                    }
-                });
-
-                notificator.for_each(|ts, _count, _notify| {
-                    let values: Vec<(V, u64)> =
-                        value_counter.iter().map(|(k, v)| (k.clone(), *v)).collect();
-                    output.session(&ts).give((ts.inner, values));
                 });
             },
         )
     }
 }
 
-impl<
-    S: Scope<Timestamp = Product<RootTimestamp, u64>>,
-    Message: SessionizableMessage,
-> CountNumFragmentsPerSessionPerEpoch<S> for Stream<S, MessagesForSession<Message>> {
+/// Calculates the top K most frequent bins per epoch.
+///
+/// This operator takes a custom `discretizer` function, i.e. a function which extracts
+/// a nominal variable (bin) `V` from a given input `K`.
+/// It reports the `top_k` bins of with the highest frequency per epoch.
+pub trait TopK<S: Scope, K, V> {
+    type Epoch: Timestamp;
+
+    /// Returns a stream of `(timestamp, (bin, frequency))`
+    fn topk<F>(&self,discretizer: F, top_k: u64,) -> Stream<S, (Self::Epoch, Vec<(V, u64)>)>
+        where F: Fn(&K) -> V + 'static;
+}
+
+impl<S, K, V, T> TopK<S, K, V> for Stream<S, K>
+    where S: Scope<Timestamp = Product<RootTimestamp, T>>,
+        K: Data,
+        V: ExchangeData + Hash + Eq + Sync,
+        T: Timestamp,
+{
+    type Epoch = T;
+
+    fn topk<F>(&self, discretizer: F, topk: u64) -> Stream<S, (Self::Epoch, Vec<(V, u64)>)>
+        where F: Fn(&K) -> V + 'static
+    {
+        // Workers compute partial counts per epoch (stage1), which are then aggregated
+        // by worker 0 to produce the global counts (stage2) for each epoch
+
+        // ship entries with the same key to the same worker
+        let exchange = Exchange::new(move |x| hash_code(x));
+
+        let mut value_counter: HashMap<T, HashMap<V, u64>> = HashMap::new();
+
+        // count number of occurrences for each entry per epoch
+        let stage1 = self.map(move |k: K| discretizer(&k)).unary_notify(
+            exchange, "histogram1", vec![],
+            move |input, output, notificator| {
+                // map: epoch -> signature -> number of occurrences in epoch
+                input.for_each(|epoch, values| {
+                    notificator.notify_at(epoch.clone());
+                    let epoch_hashmap = value_counter
+                        .entry(epoch.time().inner.clone())
+                        .or_insert_with(HashMap::new);
+                    for value in values.drain(..) {
+                        let value_count = epoch_hashmap.entry(value).or_insert(0);
+                        *value_count += 1;
+                    }
+                });
+
+                // spit counts for each epoch upon notification
+                notificator.for_each(|ts, _count, _notify| {
+                    let ov = value_counter.remove(&ts.time().inner);
+                    if let Some(v) = ov {
+                        let values: Vec<(V, u64)> = v.into_iter().collect();
+                        output.session(&ts).give(values);
+                    }
+                });
+            },
+        );
+
+        let mut aggregated_per_epoch: HashMap<T, HashMap<V, u64>> = HashMap::new();
+        // send partial counts to worker 0 and aggregate them per epoch
+        let stage2 = stage1.unary_notify(
+            Exchange::new(move |_| 0), "histogram2", vec![],
+            move |input, output, notificator| {
+                input.for_each(|epoch, values| {
+                    notificator.notify_at(epoch.clone());
+                    let epoch_data = aggregated_per_epoch
+                        .entry(epoch.time().inner.clone())
+                        .or_insert_with(HashMap::new);
+                    for value in values.drain(..) {
+                        for &(ref v, count) in &value {
+                            let x = epoch_data.entry(v.clone()).or_insert(0);
+                            *x += count;
+                        }
+                    }
+                });
+                // spit top-k results for each epoch upon notification
+                notificator.for_each(|ts, _count, _notify| {
+                    let oepoch_data = aggregated_per_epoch.remove(&ts.time().inner);
+                    if let Some(epoch_data) = oepoch_data {
+                        let mut values : Vec<(V, u64)> = epoch_data.into_iter().collect();
+                        values.sort_by(|a, b| b.1.cmp(&a.1));
+                        values.truncate(topk as usize);
+                        output.session(&ts).give((ts.inner.clone(), values));
+                    }
+                });
+            });
+
+        stage2
+    }
+}
+
+/// Counts the number of fragmented sessions observed after each epoch.
+///
+/// This is intended for debugging and validation purposes. It emits a stream
+/// of `(number of fragments per session, frequency)` tuples. If a session consists
+/// of more than a single fragment, this means that the session prematurely expired
+/// and thus the the session have been split over multiple epochs. Thus if the frequency
+/// of tuples where `number of fragments per session > 1` is high, this indicates
+/// that the expiry delay is too short for all messages to arrive in time.
+/// In such cases, it is recommended to increase the `session_time` window given to
+/// the [`Sessionize`](../trait.Sessionize.html) operator.
+pub trait CountNumFragmentsPerSessionPerEpoch<S: Scope> {
+    /// Reports the frequency of the number of fragmented sessions observed after each epoch.
+    fn count_num_fragments_per_session(&self) -> Stream<S, Vec<(u64, u64)>>;
+}
+
+impl<S: Scope, M: SessionizableMessage> CountNumFragmentsPerSessionPerEpoch<S>
+    for Stream<S, MessagesForSession<M>>
+{
     fn count_num_fragments_per_session(&self) -> Stream<S, Vec<(u64, u64)>> {
-        let exchange = Exchange::new(|messages: &MessagesForSession<Message>| {
+        let exchange = Exchange::new(|messages: &MessagesForSession<M>| {
             hash_code(&messages.session)
         });
         let mut fragment_counter: HashMap<String, u64> = HashMap::new();
-        let mut num_messages_seen = 0;
         self.unary_notify(exchange, "count_num_fragments_per_session", vec![], move |input, output, notificator| {
             input.for_each(|epoch, data| {
                 notificator.notify_at(epoch);
                 for messages_for_session in data.drain(..) {
-                    num_messages_seen += 1;
                     let count = fragment_counter.entry(messages_for_session.session.clone()).or_insert(0);
                     *count += 1;
                 }
@@ -238,7 +215,7 @@ impl<
 /// Emits total value (cumulative sum) of a numerical stream once per epoch.
 ///
 /// Equivalent to an invocation of the following, only that is uses operators built into Timely.
-/// ```
+/// ```rust
 /// stream.accumulate_by_epoch(0, |sum, data| {
 ///     for &x in data.iter() {
 ///         *sum += x;
