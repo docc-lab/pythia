dd78fafeed349057cd794fc2bd4dd9ae16e640a7
Emre Ates <ates@bu.edu>
2019-10-25 18:34:48 +0000

    Removed unnecessary lifetimes

--- src/searchspace.rs
+++ src/searchspace.rs
@@ -8,6 +8,6 @@ use crate::osprofiler::OSProfilerDAG;
 #[typetag::serde(tag = "type")]
 pub trait SearchSpace: Display {
     fn add_trace(&mut self, trace: &OSProfilerDAG);
-    fn get_entry_points<'a>(&'a self) -> Vec<&'a String>;
-    fn search<'a>(&'a self, group: &Group, edge: EdgeIndex) -> Vec<&'a String>;
+    fn get_entry_points(&self) -> Vec<&String>;
+    fn search(&self, group: &Group, edge: EdgeIndex) -> Vec<&String>;
 }
