7b14dcd065db40190bdebc827b0a83ff416a7fb0
Emre Ates <ates@bu.edu>
2020-02-26 14:04:48 -0700

    Changed the service to actually get traces

--- src/bin/pythia_server.rs
+++ src/bin/pythia_server.rs
@@ -4,22 +4,25 @@ use std::sync::{Arc, Mutex};
 
 use jsonrpc_core::*;
 use jsonrpc_http_server::*;
+use serde_json;
 
 use pythia::get_settings;
+use pythia::osprofiler::OSProfilerReader;
 
 fn main() {
     let settings = get_settings();
+    let reader = Arc::new(Mutex::new(OSProfilerReader::from_settings(&settings)));
     let mut io = IoHandler::new();
-    let count = Arc::new(Mutex::new(0));
     io.add_method("get_trace", move |a: Params| {
-        let res: Vec<i64> = a.parse().unwrap();
-        let mut sum = 0;
+        let res: Vec<String> = a.parse().unwrap();
+        let mut result = serde_json::Map::new();
         for i in res {
-            sum += i;
+            result.insert(
+                i.to_string(),
+                serde_json::to_value(reader.lock().unwrap().get_trace_from_base_id(&i)).unwrap(),
+            );
         }
-        let mut count = count.lock().unwrap();
-        *count += sum;
-        Ok(Value::String(count.to_string()))
+        Ok(Value::Object(result))
     });
 
     let address: &String = settings.get("server_address").unwrap();
