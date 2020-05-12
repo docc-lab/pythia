d89091214eb9b91ac390d558910df17e7929e284
Emre Ates <ates@bu.edu>
2020-05-12 10:05:11 -0400

    Moved binaries aroundg

--- src/bin/pythia.rs
+++ src/bin/pythia.rs
@@ -1,111 +1,144 @@
-#[macro_use]
-extern crate lazy_static;
-
-use std::thread::sleep;
-use std::time::Duration;
+use clap::{App, Arg, SubCommand};
 use std::time::Instant;
 
-use pythia::controller::OSProfilerController;
-use pythia::critical::CriticalPath;
-use pythia::grouping::GroupManager;
-use pythia::manifest::Manifest;
-use pythia::reader::reader_from_settings;
-use pythia::search::get_strategy;
-use pythia::settings::Settings;
+use pythia::{
+    disable_all, disable_tracepoint, dump_traces, enable_all, enable_skeleton, get_crit,
+    get_manifest, get_trace, group_folder, group_from_ids, make_decision,
+    measure_search_space_feasibility, read_trace_file, show_config, show_key_value_pairs,
+    show_manifest,
+};
 
-lazy_static! {
-    static ref SETTINGS: Settings = Settings::read();
-    static ref CONTROLLER: OSProfilerController = OSProfilerController::from_settings(&SETTINGS);
-    static ref MANIFEST: Manifest = Manifest::from_file(&SETTINGS.manifest_file.as_path())
-        .expect("Couldn't read manifest from cache");
-}
-
-/// Main Pythia function that runs in a loop and makes decisions
 fn main() {
     let now = Instant::now();
-    let mut reader = reader_from_settings(&SETTINGS);
-    let strategy = get_strategy(&SETTINGS, &MANIFEST, &CONTROLLER);
-    let mut groups = GroupManager::new();
-    let mut last_decision = Instant::now();
-    let mut last_gc = Instant::now();
-
-    // Enable skeleton
-    CONTROLLER.diable_all();
-    let to_enable = MANIFEST
-        .entry_points()
-        .iter()
-        .map(|&a| (a.clone(), None))
-        .collect();
-    CONTROLLER.enable(&to_enable);
-
-    println!("Enabled following tracepoints: {:?}", to_enable);
-
-    // Main pythia loop
-    loop {
-        // Collect traces, increment groups
-        let traces = reader.get_recent_traces();
-        let critical_paths = traces
-            .iter()
-            .map(|t| CriticalPath::from_trace(t).unwrap())
-            .collect();
-        groups.update(&critical_paths);
-        println!(
-            "Got {} paths of duration {:?} at time {}us",
-            traces.len(),
-            critical_paths
-                .iter()
-                .map(|p| p.duration)
-                .collect::<Vec<Duration>>(),
-            now.elapsed().as_micros()
-        );
-        println!("Groups: {}", groups);
-
-        if last_gc.elapsed() > SETTINGS.gc_epoch {
-            // Run garbage collection
-            last_gc = Instant::now();
+    let matches = App::new("Pythia")
+        .version("1.0")
+        .author("Emre Ates <ates@bu.edu>")
+        .subcommand(
+            SubCommand::with_name("manifest")
+                .arg(Arg::with_name("manifest-file").required(true).index(1))
+                .arg(Arg::with_name("overwrite").long("overwrite")),
+        )
+        .subcommand(
+            SubCommand::with_name("get-trace")
+                .arg(Arg::with_name("trace-id").required(true).index(1))
+                .arg(Arg::with_name("to-file").long("to-file")),
+        )
+        .subcommand(
+            SubCommand::with_name("group-folder")
+                .arg(Arg::with_name("trace-folder").required(true).index(1)),
+        )
+        .subcommand(
+            SubCommand::with_name("group-ids")
+                .arg(Arg::with_name("traceid-file").required(true).index(1)),
+        )
+        .subcommand(
+            SubCommand::with_name("read-file")
+                .arg(Arg::with_name("trace-file").required(true).index(1)),
+        )
+        .subcommand(
+            SubCommand::with_name("dump-traces")
+                .arg(Arg::with_name("trace-file").required(true).index(1)),
+        )
+        .subcommand(
+            SubCommand::with_name("get-crit")
+                .arg(Arg::with_name("trace-id").required(true).index(1)),
+        )
+        .subcommand(
+            SubCommand::with_name("key-value")
+                .arg(Arg::with_name("trace-id").required(true).index(1)),
+        )
+        .subcommand(
+            SubCommand::with_name("diagnose")
+                .arg(Arg::with_name("epoch-file").required(true).index(1))
+                .arg(
+                    Arg::with_name("budget")
+                        .long("budget")
+                        .short("b")
+                        .takes_value(true),
+                )
+                .arg(Arg::with_name("dry-run").long("dry-run")),
+        )
+        .subcommand(SubCommand::with_name("disable-all"))
+        .subcommand(
+            SubCommand::with_name("disable-tracepoint")
+                .arg(Arg::with_name("tracepoint-id").required(true).index(1)),
+        )
+        .subcommand(
+            SubCommand::with_name("try-manifest")
+                .arg(Arg::with_name("trace-file").required(true).index(1)),
+        )
+        .subcommand(
+            SubCommand::with_name("show-manifest")
+                .arg(Arg::with_name("request-type").required(true).index(1)),
+        )
+        .subcommand(SubCommand::with_name("enable-all"))
+        .subcommand(SubCommand::with_name("enable-skeleton"))
+        .subcommand(SubCommand::with_name("show-config"))
+        .subcommand(SubCommand::with_name("run"))
+        .get_matches();
+    match matches.subcommand() {
+        ("manifest", Some(matches)) => {
+            get_manifest(
+                matches.value_of("manifest-file").unwrap(),
+                matches.occurrences_of("overwrite") > 0,
+            );
         }
-
-        if last_decision.elapsed() > SETTINGS.decision_epoch {
-            // Make decision
-            let mut budget = SETTINGS.tracepoints_per_epoch;
-            let problem_groups = groups.problem_groups();
-            println!("Making decision. Top 10 problem groups:");
-            for g in problem_groups.iter().take(10) {
-                println!("{}", g);
-            }
-            for &g in problem_groups.iter() {
-                let problem_edges = g.problem_edges();
-
-                println!("Top 10 edges of group {}:", g);
-                for edge in problem_edges.iter().take(10) {
-                    let endpoints = g.g.edge_endpoints(*edge).unwrap();
-                    println!(
-                        "({} -> {}): {}",
-                        g.g[endpoints.0], g.g[endpoints.1], g.g[*edge]
-                    );
-                }
-                for &edge in problem_edges.iter() {
-                    if budget <= 0 {
-                        break;
-                    }
-                    let decisions = strategy
-                        .search(g, edge, budget)
-                        .iter()
-                        .take(budget)
-                        .map(|&t| (t, g.request_type))
-                        .collect::<Vec<_>>();
-                    println!("Enabling {:?}", decisions);
-                    budget -= decisions.len();
-                    CONTROLLER.enable(&decisions);
-                }
-                if budget <= 0 {
-                    break;
-                }
-            }
-
-            last_decision = Instant::now();
+        ("group-folder", Some(matches)) => {
+            group_folder(matches.value_of("trace-folder").unwrap());
         }
-
-        sleep(SETTINGS.jiffy);
-    }
+        ("group-ids", Some(matches)) => {
+            group_from_ids(matches.value_of("traceid-file").unwrap());
+        }
+        ("read-file", Some(matches)) => {
+            read_trace_file(matches.value_of("trace-file").unwrap());
+        }
+        ("try-manifest", Some(matches)) => {
+            measure_search_space_feasibility(matches.value_of("trace-file").unwrap());
+        }
+        ("show-manifest", Some(matches)) => {
+            show_manifest(matches.value_of("request-type").unwrap());
+        }
+        ("dump-traces", Some(matches)) => {
+            dump_traces(matches.value_of("trace-file").unwrap());
+        }
+        ("get-trace", Some(matches)) => {
+            get_trace(
+                matches.value_of("trace-id").unwrap(),
+                matches.occurrences_of("to-file") > 0,
+            );
+        }
+        ("get-crit", Some(matches)) => {
+            get_crit(matches.value_of("trace-id").unwrap());
+        }
+        ("disable-tracepoint", Some(matches)) => {
+            disable_tracepoint(matches.value_of("tracepoint-id").unwrap());
+        }
+        ("key-value", Some(matches)) => {
+            show_key_value_pairs(matches.value_of("trace-id").unwrap());
+        }
+        ("diagnose", Some(matches)) => {
+            make_decision(
+                matches.value_of("epoch-file").unwrap(),
+                matches.occurrences_of("dry-run") > 0,
+                match matches.value_of("budget") {
+                    Some(x) => x.parse::<usize>().ok().unwrap(),
+                    None => 0,
+                },
+            );
+        }
+        ("disable-all", Some(_)) => {
+            disable_all();
+        }
+        ("enable-all", Some(_)) => {
+            enable_all();
+        }
+        ("enable-skeleton", Some(_)) => {
+            enable_skeleton();
+        }
+        ("show-config", Some(_)) => {
+            show_config();
+        }
+        _ => panic!("Must provide a subcommand, see --help for commands"),
+    };
+    eprintln!("Overall Pythia took {}us", now.elapsed().as_micros());
 }
