7478f7f7ef29cff0af202605178d1da0bf124aca
Alex Ellis <ellisonjtk@gmail.com>
2021-03-18 22:32:47 -0400

    Added comments to search strategies

--- src/search/flat.rs
+++ src/search/flat.rs
@@ -19,16 +19,22 @@ pub struct FlatSearch {
 
 impl SearchStrategy for FlatSearch {
     fn search(&self, group: &Group, edge: EdgeIndex, budget: usize) -> Vec<TracepointID> {
+        // Get all of the HierarchicalCriticalPaths that contain this group
         let matches = self.manifest.find_matches(group);
         let mut result = HashSet::new();
         for m in matches {
             let now = Instant::now();
+            // Note: it looks like this is greedy, so it'll take as many as possible from the first match!
+            // So the later matches might be ignored. I wonder if the order of find_matches should matter?
             let remaining_budget = budget - result.len();
             result.extend(
                 self.split_group_by_n(m, group, edge, remaining_budget)
                     .iter()
                     .take(remaining_budget),
             );
+            // note: no guarantee (that I saw) that we actually get N=remaining_budget items from the group, but it
+            // doesn't really matter because budget is updated in pythia_controller.rs.
+            // We may be getting under the budget (due to rounding)
             eprintln!("Finding middle took {}", now.elapsed().as_micros(),);
             result = result
                 .into_iter()
@@ -50,9 +56,13 @@ impl FlatSearch {
     /// Find n tracepoints that equally separate the edge according to the path
     fn split_group_by_n(
         &self,
-        path: &HierarchicalCriticalPath, // Contains full search space
+        // path "contains full search space"
+        // Alex note: really the path is the full possible trace path with all tracepoints
+        path: &HierarchicalCriticalPath,
         group: &Group,
         edge: EdgeIndex,
+        // Alex note: n is approximate. Due to rounding (etc), we may add n-1.
+        // For example, with nodes_between and n=3, we only add 2 trace points because of gap math.
         n: usize,
     ) -> Vec<TracepointID> {
         let mut result = Vec::new();
@@ -62,13 +72,16 @@ impl FlatSearch {
         let mut nodes_between = 0;
         let mut cur_path_idx = path.start_node;
         let mut cur_group_idx = group.start_node;
+        // Figure out how many nodes are in between the source node and target node
         loop {
             if path.g[cur_path_idx] == group.g[cur_group_idx] {
                 if cur_group_idx == source {
+                    // We're already setting cur_path_idx to path.start_node so not sure why we need the next line
                     path_source = cur_path_idx;
                     nodes_between = 0;
                 }
                 if cur_group_idx == target {
+                    // path_target ends up as the target
                     path_target = cur_path_idx;
                     nodes_between -= 1;
                     break;
@@ -81,19 +94,33 @@ impl FlatSearch {
         if nodes_between == 0 {
             println!("The matching nodes are consecutive");
         }
+        // Keep a vector of... gaps?
         let mut gaps = Vec::new();
         if nodes_between <= n {
             for _ in 0..nodes_between {
+                // If we have fewer nodes between than nodes we want to turn on, we don't need any gaps (AKA gaps of 0)
+                // e.g. between A and D in A->B->C->D and we want to turn on 3. We can just turn on B and C.
                 gaps.push(0);
             }
         } else {
             for _ in 0..n {
+                // Otherwise we just need n gaps of length / n + 1
+                // e.g. between A and G in
+                // A -> B -> C -> D -> E -> F -> G
+                // nodes_between = 5
+                // If n=3, we wants 3 gaps of 1 (5 / 3 = 1 with integer division)
                 gaps.push(nodes_between / (n + 1));
             }
         }
+        // This sets the cur_path_idx to one after the path source. (probably because of assertion at bottom).
+        // e.g. between A and G in
+        // A -> B -> C -> D -> E -> F -> G
+        // cur_path_idx would be B. I guess potentially missing one is better than double counting first.
         cur_path_idx = path.next_node(path_source).unwrap();
+        // For all of the gaps:
         for i in gaps {
             for _ in 0..i {
+                // Walk through the path for the gap number
                 cur_path_idx = path.next_node(cur_path_idx).unwrap();
                 if cur_path_idx == path_target {
                     eprintln!("Reached target prematurely");
@@ -121,6 +148,7 @@ impl FlatSearch {
                     continue;
                 }
             }
+            // Note: I think we'll reach this after the "reached prematurely" check above (that break is only out of 0..i loop)
             if cur_path_idx == path_target {
                 println!("Already reached target node, breaking");
                 continue;
--- src/search/hierarchical.rs
+++ src/search/hierarchical.rs
@@ -22,8 +22,14 @@ impl SearchStrategy for HierarchicalSearch {
     fn search(&self, group: &Group, edge: EdgeIndex, budget: usize) -> Vec<TracepointID> {
         let mut rng = &mut rand::thread_rng();
         let (source, target) = group.g.edge_endpoints(edge).unwrap();
+        // Get context for source and target, aka the hierarchical nodes that come before them.
         let source_context = self.get_context(group, source);
         let target_context = self.get_context(group, target);
+        // Build a common context of the shared prefix between those two contexts.
+        // e.g.
+        // source_context = [1, 2, 3, 4]
+        // target_context = [1, 2]
+        // common_context = [1, 2]
         let mut common_context = Vec::new();
         let mut idx = 0;
         loop {
@@ -61,11 +67,20 @@ impl HierarchicalSearch {
         matches: &Vec<&HierarchicalCriticalPath>,
         context: Vec<TracepointID>,
     ) -> Vec<TracepointID> {
+        // Really we're looking at possible new parents that are at the correct hierarchy
+        // depth. Once we have them, we'll return THEIR children as the search result.
         let mut possible_child_nodes = Vec::new();
+        // We want to iterate through every path that contains the group in question.
         for m in matches {
+            // We want to keep a running list of (hierarchy depth, possible node) tuples
+            // Initialize with 0 and no possible node. Note that the first in last out order doesn't matter, as there
+            // aren't any shared variables between the entries.
             let mut possible_next_nodes: Vec<(usize, Option<NodeIndex>)> = vec![(0, None)];
             while !possible_next_nodes.is_empty() {
                 let to_eval = possible_next_nodes.pop().unwrap();
+                // If we've reached the depth in the hierarchy of the shared context, that means the possible
+                // node we have is at the current deepest level in the hierarchy, and thus we can add it to the possible
+                // new parent nodes.
                 if to_eval.0 == context.len() {
                     possible_child_nodes.push((
                         m,
@@ -76,10 +91,19 @@ impl HierarchicalSearch {
                     ));
                     continue;
                 }
+                // If we're not at the depth of the shared context, we want to recursively add this next node's children.
+                // Kind of like doing BFS, add it to the queue.
+
+                // First, get the tracepoint at this point in the shared context. This will ensure that we're following
+                // the shared context as we go hierarchically deeper into the traces.
+                // Note that this is either the first one (for the first iteration) or the next one
                 let tracepoint = context[to_eval.0];
                 let nidx = to_eval.1;
                 match nidx {
                     None => {
+                        // I think this is only matched at the beginning, when the possible node is None. In this case,
+                        // we want to add the node at the beginning of the hierarchy that is the first element in
+                        // the shared context.
                         for &nidx in m.hierarchy_starts.iter() {
                             if m.g[nidx].tracepoint_id == tracepoint {
                                 possible_next_nodes.push((to_eval.0 + 1, Some(nidx)))
@@ -87,6 +111,9 @@ impl HierarchicalSearch {
                         }
                     }
                     Some(nidx) => {
+                        // We want to explore the child nodes of the current node (in the hierarchical path match) that
+                        // match the tracepoint. This is how we add the next layer to the queue as we go down the
+                        // hierarchy, and we have the tracepoint id match to make sure we're following the context.
                         for candidate in m
                             .child_nodes(nidx)
                             .iter()
@@ -98,6 +125,8 @@ impl HierarchicalSearch {
                 }
             }
         }
+        // We now have a list of all of the vanguard nodes, if you will, at the current deepest level of the hierarchy.
+        // We now want to add all of their children to the results, which we get from child_nodes.
         let mut result = HashSet::new();
         for (path, nidx) in possible_child_nodes {
             for child in path.child_nodes(nidx) {
@@ -107,6 +136,11 @@ impl HierarchicalSearch {
         result.drain().collect()
     }
 
+    // get_context walks through the group's nodes and builds a list of the hierarchical tracepoints that come before the node.
+    // For example, with group:
+    // A Entry -> B Entry -> C Entry -> C Exit -> D Entry
+    // and node: (D entry)
+    // Would build context for tracepoints from (A Entry, B Entry).
     fn get_context(&self, group: &Group, node: NodeIndex) -> Vec<TracepointID> {
         let mut result = Vec::new();
         let mut nidx = group.start_node;
