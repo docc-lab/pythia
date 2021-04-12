8272e6004d4f9e2dfdf215daaeb71cefd6c2b8e6
Mert TOSLALI <toslali@bu.edu>
2021-04-12 10:48:27 -0400

    Add edge durations for postmortem analysis -- eval metrics

--- src/grouping.rs
+++ src/grouping.rs
@@ -63,12 +63,13 @@ impl Display for GroupEdge {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "Edge({} elements, {:?} min, {:?} max, {:?} variance, {:?} mean)",
+            "Edge({} elements, {:?} min, {:?} max, {:?} variance, {:?} mean, durations: {:?})",
             self.duration.len(),
             self.duration.iter().min().unwrap(),
             self.duration.iter().max().unwrap(),
             variance(self.duration.iter().map(|&x| x.as_nanos())),
             mean(self.duration.iter().map(|&x| x.as_nanos())),
+            self.duration.iter().collect::<Vec<_>>()
         )
     }
 }
@@ -173,7 +174,7 @@ impl Group {
     /// Returns all edges sorted by variance.
     pub fn problem_edges(&self) -> Vec<EdgeIndex> {
         let mut edge_variances = HashMap::<EdgeIndex, f64>::new();
-        let mut edge_means = HashMap::<EdgeIndex, f64>::new();
+        // let mut edge_means = HashMap::<EdgeIndex, f64>::new();
         let mut cur_node = self.start_node;
         let mut prev_node = None;
         loop {
@@ -183,11 +184,6 @@ impl Group {
                         edge_variances.insert(
                             edge,
                             variance(self.g[edge].duration.iter().map(|d| d.as_secs_f64())),
-                        );
-
-                        edge_means.insert(
-                            edge,
-                            mean(self.g[edge].duration.iter().map(|d| d.as_secs_f64())),
                         );
                     }
                     None => panic!("No edge?"),
